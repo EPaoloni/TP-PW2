7beed466904cd376466a86a3227e243e916661ea
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2019-10-03 19:49:08 -0300

    script creacion tabla usuario

---
+++ Scripts SQL/usuario.sql
@@ -0,0 +1,12 @@
+//Creacion de tabla
+
+USE pw2;
+
+CREATE TABLE usuario(
+    id INT AUTO_INCREMENT,
+    username VARCHAR(30),
+    pass VARCHAR(30),
+    nombre VARCHAR(30),
+    apellido VARCHAR(30),
+    PRIMARY KEY(id)
+)
