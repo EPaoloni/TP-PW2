ace6b42a9e227cb7cf909d5e108f5b70bb315782
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2019-10-06 17:52:54 -0300

    Refactor del header

--- registro.php
+++ registro.php
@@ -1,15 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Registro</title>
 
-    <script src="../StaticContent/jquery-3.4.1.min.js"></script>
-    <script src="../StaticContent/bootstrap-4.3.1-dist/js/bootstrap.js"></script>
-    <link rel="stylesheet" href="../StaticContent/bootstrap-4.3.1-dist/css/bootstrap.css">
-    <link rel="stylesheet" href="../StaticContent/css/style-login.css">
+    <?php include("Vistas/header.html"); ?>
+
 </head>
 <body>
     <h1 id="login-title" class="text-center">Formulario de registro</h1>
