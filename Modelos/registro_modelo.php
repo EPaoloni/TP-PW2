798a6fda52a79d221b88c5b8d41d764fb2fcbffa
Alejo <alejozonta99@gmail.com>
2019-10-15 00:35:38 -0300

    Arreglos en login y cambios en el registro

--- Modelos/registro_modelo.php
+++ Modelos/registro_modelo.php
@@ -6,12 +6,43 @@
         
         $conexion = getConexion();
 
-        $sql = "INSERT INTO usuario(username, pass, nombre, apellido)
-                    VALUES('" . $username . "', '" . $password . "', '" . $nombre . "', '" . $apellido . "');";
+        $sql = "INSERT INTO usuario(nombre, apellido)
+                    VALUES( '" . $nombre . "', '" . $apellido . "');";
         $result = mysqli_query($conexion, $sql);
 
         mysqli_close($conexion);
+
+        if ($result){
+            $result=registrarCredencialPorUsuario($username, $password, $nombre, $apellido);
+        }
         return $result;
+    }
+
+    function registrarCredencialPorUsuario($username, $password, $nombre, $apellido){
+        $idUsuario= getIdUsuario($nombre, $apellido);
+        $conexion = getConexion();
+
+        $sql = "INSERT INTO credencial(username, pass,usuario)
+                        VALUES('" . $username . "', '" . $password . "', '" . $idUsuario . "' );";
+        $result = mysqli_query($conexion, $sql);
+
+
+        mysqli_close($conexion);
+
+        return $result;
+    }
+
+    function getIdUsuario($nombre, $apellido){
+
+        $conexion = getConexion();
+
+        $sql = "SELECT id FROM usuario WHERE nombre = '" . $nombre . "' AND  apellido =  '" . $apellido . "' ;";
+        $result = mysqli_query($conexion, $sql);
+        $idBuscada= mysqli_fetch_assoc($result);
+
+        mysqli_close($conexion);
+
+        return $idBuscada['id'];
     }
 
 ?>
