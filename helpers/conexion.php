6f518dde783601cc7383f53ffc88cadef2d6a1f6
Ezequiel <Paoloni>
2019-10-20 01:14:37 -0300

    Se hace la conexion orientada a objetos

--- helpers/conexion.php
+++ helpers/conexion.php
@@ -10,10 +10,10 @@ function getConexion(){
     $dbname = $config['database']['database'];
     $password = $config['database']['password'];
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
 
     return $conn;
