d7f17f78a7eaef4cd15c903af55577ef0f4df17a
Ezequiel <Paoloni>
2019-10-20 02:01:26 -0300

    Se crea la clase Query con los metodos Consulta e Insert

---
+++ helpers/Query.php
@@ -0,0 +1,89 @@
+<?php
+
+include_once("helpers/conexion.php");
+include_once("helpers/Logger.php");
+
+class Query{
+
+    public function __constructor(){
+    }
+
+    /**
+     * -----------------------------------
+     * Realiza una consulta a la base de datos definida en conexion.php. Basicamente arma un string concatenando los parametros recibidos.
+     * Si no se encontraron registros coincidentes devuelve null.
+     * -----------------------------------
+     * @param string $columnas Si viene vacio hace un SELECT *.
+     * @param string $tablas Lo que va despues del FROM. Si se requiere un INNER JOIN debe venir la sentencia completa.
+     * @param string $condiciones Lo que va despues del WHERE.
+     */
+    public function consulta($columnas, $tablas, $condiciones){
+        $conn = $this->establecerConexion();
+
+        if(isset($condiciones)){
+            $sql = "SELECT " . $columnas . " FROM " . $tablas . " WHERE " . $condiciones;
+        } else {
+            $sql = "SELECT " . $columnas . " FROM " . $tablas;
+        }
+        $result = $conn->query($sql);
+    
+        if($result === false){
+            $log = new Logger();
+            $log->error("Ocurrio un error en la base de datos al realizar la consulta: $sql \t El error de la base de datos es: $conn->error");
+            $conn->close();
+            exit(1);
+        }
+        if ($result->num_rows > 0) {
+            $entradas = $result->fetch_assoc();
+        } else {
+            $entradas = null;
+        }
+        $result->close();
+        $conn->close();
+
+        return $entradas;
+    }
+
+        /**
+     * -----------------------------------
+     * Realiza una consulta a la base de datos definida en conexion.php. Basicamente arma un string concatenando los parametros recibidos.
+     * Devuelve true si la insercion es exitosa.
+     * -----------------------------------
+     * @param string $tabla
+     * @param string $valores Los valores a insertar, entre parentesis, si son varias entradas poner varios parentesis.
+     * @param string $columnas Las columnas que se quiere afectar.
+     */
+    public function insert($tabla, $valores, $columnas){
+        $conn = $this->establecerConexion();
+
+        $sql = "INSERT INTO " . $tabla . $columnas . " VALUES" . $valores;
+
+        $result = $conn->query($sql);
+
+        if($result){
+            $insertExitoso = true;
+        } else {
+            $log = new Logger();
+            $log->error("Ocurrio un error en la base de datos al realizar el insert: $sql \t El error de la base de datos es: $conn->error");
+            $insertExitoso = false;
+        }
+        
+        $conn->close();
+
+        return $insertExitoso;
+    }
+
+    private function establecerConexion(){
+        $conn = getConexion();
+        
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+
+}
+
+?>
