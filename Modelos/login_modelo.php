fb7796ebb33cd2ecd761855983a2a5c6aabc172f
Ezequiel <Paoloni>
2019-11-03 17:40:32 -0300

    Agrego mail a la session

--- Modelos/login_modelo.php
+++ Modelos/login_modelo.php
@@ -6,12 +6,13 @@
     function validarCredencialesUsuario($username, $password){
         
         $query = new Query();
-        $resultado = $query->consulta("usuario.idUsuario", "usuario INNER JOIN Credencial ON Usuario.numeroCredencialUsuario=Credencial.idCredencial",
+        $resultado = $query->consulta("usuario.idUsuario, usuario.mail", "usuario INNER JOIN Credencial ON Usuario.numeroCredencialUsuario=Credencial.idCredencial",
                                                     "username = '$username' and pass = '$password'");
         if($resultado>0){
             
             session_start();
             $_SESSION['username'] = $username;
+            $_SESSION['emailUsuario'] = $resultado[0]['mail'];
         }
         return $resultado;
     }
