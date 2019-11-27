a4fe66443645bbb2c51826cb14cca0308ad8f71a
Alejo <alejozonta99@gmail.com>
2019-11-27 14:29:22 -0300

    Se agregan forms para busqueda y button buscar todas

--- listaPagos.php
+++ listaPagos.php
@@ -5,8 +5,31 @@ include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/factura_modelo.php");
 
 
 checkIsAdmin();
+$errorFechas="";
+$errorUsername="";
+if(isset($_GET['submit-form-fechas'])){
+    $fechaDesde = isset($_GET['inputFechaDesde']) ? $_GET['inputFechaDesde'] : null ;
+    $fechaHasta = isset($_GET['inputFechaHasta']) ? $_GET['inputFechaHasta'] : null ;
+    if($_GET['inputFechaDesde']!= null && $_GET['inputFechaHasta']!= null ){
+        $listaFacturas = getFacturasByFechas($fechaDesde,$fechaHasta);
+    }else{
+        $listaFacturas=getFacturas();;
+        $errorFechas="<p class='text-danger text-justify'>Ocurrio un error porque alguno de los campos esta vacio</p>";
+    }
+} else{
+    if(isset($_GET['submit-form-username'])){
+        $username=isset($_GET['inputUsername']) ? $_GET['inputUsername'] : null ;
+        if($_GET['inputUsername'] != null){
+            $listaFacturas = getFacturasByUsername($username);
+        }else{
+            $listaFacturas=getFacturas();;
+            $errorUsername="<p class='text-danger text-justify'>Ocurrio un error porque el campo esta vacio </p>";
+        }
+    }else{
+        $listaFacturas = getFacturas();
+    }
+} 
 
-$listaFacturas = getFacturas();
 
 
 ?>
@@ -19,6 +42,36 @@ $listaFacturas = getFacturas();
 </head>
 <body>
 <?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/header.php"); ?>
+<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col col-lg-4">
+            <form class="form-row align-items-center" action="./listaPagos.php" >
+                <div class="form-group">
+                <h2 class="text-success"  >Buscar por Fechas</h2>
+                    <label for="inputFechaDesde">Fecha desde:</label>
+                    <input type="date" class="form-control" id="inputFechaDesde" name="inputFechaDesde">
+                    <label for="inputFechaHasta">Fecha hasta:</label>
+                    <input type="date" class="form-control" id="inputFechaHasta" name="inputFechaHasta">
+                    <input type="submit" name="submit-form-fechas" class="btn btn-primary mb-2 mt-4" value="Aceptar">
+                    <?php echo $errorFechas;?>
+                </div>
+            </form>
+        </div>
+        <div class="col col-lg-2">
+        </div>
+        <div class="col col-lg-4">
+            <form class="form-row align-items-center" action="./listaPagos.php" >
+                <div class="form-group">
+                    <h2 class="text-success"  >Buscar por Username</h2>
+                    <label for="inputUsername">Username: </label>
+                    <input type="txt" class="form-control" id="inputUsername" name="inputUsername">
+                    <input type="submit" name="submit-form-username" class="btn btn-primary mb-2 mt-4" value="Aceptar">
+                    <?php echo $errorUsername;?> 
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <div class="container">
     <table class="table">
         <thead class="thead-dark">
@@ -75,7 +128,8 @@ $listaFacturas = getFacturas();
             ?>
         </tbody>
     </table>
-    <btn class="btn btn-primary" onclick="window.print()"> Imprimir</btn>
+    <a href="./listaPagos.php" class="btn btn-success mb-2 mt-4">Buscar todos</a>
+    <btn class="btn btn-primary mb-2 mt-4" onclick="window.print()"> Imprimir</btn>
 </div>
 
 <?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/footer.php"); ?>
