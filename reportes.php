91429ad2e3816dc16455fb65d5f1acc8c52d1de3
Alejo <alejozonta99@gmail.com>
2019-11-30 02:46:32 -0300

    Agrego tasa Ocupacion vuelo

--- reportes.php
+++ reportes.php
@@ -18,8 +18,8 @@ checkIsAdmin();
 <div class="container">
     <a class="btn btn-primary" href="./listaPagos.php">Ver pagos de usuarios</a>
     <a class="btn btn-primary" href="./cabinaMasVendida.php">Cabina Mas Vendida</a>
-    <a class="btn btn-primary" href="./index.php">ASD</a>
-    <a class="btn btn-primary" href="./listaReservas.php">ASD</a>
+    <a class="btn btn-primary" href="./tasaOcupacionVuelo.php">Tasa de ocupación por viaje </a>
+    <a class="btn btn-primary" href="./tasaOcupacionEquipo.php">Tasa de ocupación equipo</a>
 </div>
     
 <?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/footer.php"); ?>
---
+++ tasaOcupacionVuelo.php
@@ -0,0 +1,67 @@
+<?php
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/admin_modelo.php");
+    include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/tasaOcupacion_modelo.php");
+    checkIsAdmin();
+    $error="";
+    $listaVuelosOcupacion = getListadoVuelosOcupacion();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("Vistas/head.html"); ?>
+</head>
+<body>
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/header.php"); ?>
+<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col col-lg-2">
+        </div>
+        <div class="col col-lg-4">
+            <form class="form-row align-items-center" action="#" >
+                    <div class="form-group">
+                    <h2 class="text-success"  >Buscar por vuelo</h2>
+                        <label for="input-vuelo">N° de vuelo:</label>
+                        <input type="txt" class="form-control" id="input-vuelo" name="input-vuelo">
+                        <input type="submit" name="submit-form" class="btn btn-primary mb-2 mt-4" value="Aceptar">
+                        <?php echo $error;?>
+                    </div>
+                </form>
+        </div>
+        <div class="col col-lg-2">
+        </div>
+    </div>
+</div>
+<div class="container">
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Vuelo</th>
+                <th scope="col">Tasa de ocupacion</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if($listaVuelosOcupacion<>null){
+                foreach ($listaVuelosOcupacion as $vuelo ) {
+            ?>
+
+            <tr>
+                <th scope="row"><?php echo $vuelo['idVuelo'] ?></th>
+                <th><?php echo $vuelo['tasa']   ?>%</th>
+               
+            </tr>
+        
+            <?php      
+               }
+            }
+            ?>
+        </tbody>
+    </table>
+    <a href="./tasaOcupacionVuelo.php" class="btn btn-success mb-2 mt-4">Buscar todos</a>
+</div>
+
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/footer.php"); ?>
+</body>
+</html>
