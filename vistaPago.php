99fd9f55783a57ef6d770d10b2a88310b7988263
Ian Tries <ian_tries@hotmail.com>
2019-12-02 19:55:47 -0300

    Refactor logica vistaPago

--- vistaPago.php
+++ vistaPago.php
@@ -1,61 +1,14 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/helpers/Query.php");
-include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/usuario_modelo.php");
-include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/busqueda_modelo.php");
-include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/reserva_modelo.php");
+include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Modelos/pago_modelo.php");
 
 session_start();
 
 $numeroDeReserva= $_GET['idReserva'];
 
-
 verificarCodigosDeViajero($numeroDeReserva);
 
-function verificarCodigosDeViajero($numeroDeReserva){
-    $query = new Query();
-    $idTitular = $query->consulta("idTitular",
-                                    "reserva",
-                                    "idReserva = " . $numeroDeReserva);
-
-    $idAcompaniantes = $query->consulta("idUsuario",
-                                        "acompaniante_reserva",
-                                        "idReserva = " . $numeroDeReserva);
-
-    $idPasajeros[] = $idTitular[0]['idTitular'];
-
-    if($idAcompaniantes != null){
-        foreach ($idAcompaniantes as $idAcompaniante) {
-            $idPasajeros[] = $idAcompaniante['idUsuario'];
-        }
-    }
-    $reserva = getReservaById($numeroDeReserva);
-    $idVuelo = $reserva['idVuelo'];
-    $vuelo = consultarVueloPorId($idVuelo);
-    $codigoViajeroRequerido = $vuelo['codigoViajeroRequerido'];
-    foreach ($idPasajeros as $idPasajero) {
-        $codigoViajeroPasajero = checkCodigoViajero($idPasajero);
-        if($codigoViajeroPasajero == 0){
-            $_SESSION['errorCodigoDeViajero'] = "Alguno de los pasajeros no tiene asignado un codigo de viajero en la reserva numero: $numeroDeReserva";
-            header("location: ./listaReservas.php");
-        }
-        if($codigoViajeroRequerido > $codigoViajeroPasajero){
-            $_SESSION['errorCodigoDeViajero'] = "Alguno de los pasajeros no cumple con el codigo de viajero requerido para la nave en la reserva numero: $numeroDeReserva";
-            $query = new Query();
-            $updateRealizado = $query->update("reserva", "reservaCaida = true", "idReserva = $numeroDeReserva");
-            header("location: ./listaReservas.php");
-        }
-    }
-}
-
-$query = new Query();
-$resultado= $query->consulta("montoReserva",
-"Reserva",
-"idReserva = '$numeroDeReserva'");
-
-$montoReserva = $resultado[0]['montoReserva'];
-
-
+$montoReserva = obtenerMontoReserva($numeroDeReserva);
 
 ?>
 
