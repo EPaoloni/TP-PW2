01a7b6c2eac81526c7cd0abb3ef63468d8ac2e1d
Ian Tries <ian_tries@hotmail.com>
2019-12-02 19:33:08 -0300

    Se elimina la vista welcome.php

--- Vistas/welcome.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/head.html"); ?>
-</head>
-<body>
-    <h1>Usuario logueado</h1>
-</body>
-</html>
