8a81603e7901eaf531d647a7afb8b518c05d4978
Alejo <alejozonta99@gmail.com>
2019-12-03 01:44:57 -0300

    Estilos vista pago

--- vistaPago.php
+++ vistaPago.php
@@ -28,33 +28,52 @@ $montoReserva = obtenerMontoReserva($numeroDeReserva);
         $_SESSION['errorPago'] = ""; 
     }
 ?>
-<form action="./Endpoints/realizarPago.php" class="container" id="form-pago">
+<div class="row justify-content-center">
+    <h2 class="text-primary mb-4">Realiza tu pago!</h2>
+    <div class="w-100"></div>
+    <form action="./Endpoints/realizarPago.php" class="container" id="form-pago">
+        <div class="form-group row">
+            <label for="" class="text-secondary col-sm-2 col-form-label">Nombre:</label>
+            <div class="col-sm-10">
+                <input type="text" name="nombre" class="form-control">
+            </div>
+        </div>
 
-    <label for="">Nombre:</label>
-    <input type="text" name="nombre" class="form-control">
+        <div class="form-group row">
+            <label for="" class="text-secondary col-sm-2 col-form-label">Apellido:</label>
+            <div class="col-sm-10">
+                <input type="text" name="apellido" class="form-control">
+            </div>
+        </div>
 
-    <label for="">Apellido:</label>
-    <input type="text" name="apellido" class="form-control">
+        <div class="form-group row">
+            <label for="" class="text-secondary col-sm-2 col-form-label">N° de tarjeta:</label>
+            <div class="col-sm-10">
+                <input type="text" name="numeroTarjeta" class="form-control border" id="numero-tarjeta">
+            </div>
+        </div>
 
-    <label for="">N° de tarjeta:</label>
-    <input type="text" name="numeroTarjeta" class="form-control border" id="numero-tarjeta">
+        <div class="form-group row">
+            <label for="" class="text-secondary col-sm-2 col-form-label">Fecha de caducidad:</label>
+            <div class="col-sm-10">
+                <input type="text" name="fechaCaducidad" class="form-control" id="fecha-caducidad">
+            </div>
+        </div>
 
-    <label for="">Fecha de caducidad:</label>
-    <input type="text" name="fechaCaducidad" class="form-control" id="fecha-caducidad">
-
-    <label for="">Codigo de seguridad:</label>
-    <input type="number" name="codigoSeguridad" class="form-control">
-
-    <div class="form-group row">
-        <label for="" class="col-sm-6 col-form-label">Monto total a abonar:</label>
-        <input type="text" name="montoPago" class="form-control col-sm-3" id="" value="<?php echo"$montoReserva";?>" readonly>
-        <input type="submit" id="boton-pagar" class="btn btn-primary float-right col-sm-2" value="Pagar">
-    </div>
-
-
-    <input type="hidden" name="idReserva" value="<?php echo $numeroDeReserva ; ?>">
-
-</form>
+        <div class="form-group row">
+            <label for="" class="text-secondary col-sm-2 col-form-label">Codigo de seguridad:</label>
+            <div class="col-sm-10">
+                <input type="number" name="codigoSeguridad" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="" class="col-sm-6 col-form-label text-secondary">Monto total a abonar:</label>
+            <input type="text" name="montoPago" class="form-control col-sm-3" id="" value="<?php echo"$montoReserva";?>" readonly>
+            <input type="submit" id="boton-pagar" class="btn btn-success float-right col-sm-2 ml-5" value="Pagar">
+        </div>
+        <input type="hidden" name="idReserva" value="<?php echo $numeroDeReserva ; ?>">
+    </form>
+</div>
 
 <?php include_once($_SERVER["DOCUMENT_ROOT"] . "/TP-PW2/Vistas/footer.php"); ?>
 </body>
